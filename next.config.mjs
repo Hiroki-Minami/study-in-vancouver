7c6c1a3009e41aee0101202fe5544f54d8de0967
Hiroki Minami <hirokimnm6@gmail.com>
2024-10-20 16:07:15 +0900

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/study-in-vancouver",
-  assetPrefix: "/study-in-vancouver/",
 };
 
 export default nextConfig;
